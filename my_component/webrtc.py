25a608a6f2060aa6b11a3f38e97e7fc695dfb490
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2021-01-06 21:46:45 +0900

    Fix WebRtcWorker to close the connection properly

--- my_component/webrtc.py
+++ my_component/webrtc.py
@@ -73,6 +73,7 @@ class WebRtcWorker:
         try:
             loop.run_forever()
         finally:
+            loop.run_until_complete(self.pc.close())
             loop.run_until_complete(loop.shutdown_asyncgens())
             loop.close()
 
