1993529916f8ec5d1ab30e3599d0ce8bc4637fb8
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2021-01-10 02:53:04 +0900

    Remove unnecessary bootstrap.css and fix frontend style

--- streamlit_webrtc/frontend/src/index.tsx
+++ streamlit_webrtc/frontend/src/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import WebRtcStreamer from "./WebRtcStreamer";
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          // Unset the background-color since <CssBaseLine /> applies the default Material Design background color
+          // (https://material-ui.com/components/css-baseline/#approach),
+          // which however does not match the Streamlit's background.
+          backgroundColor: "initial",
+        },
+      },
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <WebRtcStreamer />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <WebRtcStreamer />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
