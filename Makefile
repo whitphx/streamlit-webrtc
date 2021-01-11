b04b0bd777e9aa10e9858b7a2cb69b4bc4ab72e0
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2021-01-11 18:20:30 +0900

    Add release check (#9)

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 pkg/build:
+	python release_check.py streamlit_webrtc/__init__.py
 	cd streamlit_webrtc/frontend && npm run build
 	poetry build
---
+++ release_check.py
@@ -0,0 +1,48 @@
+""" A script to check whether a variable `_RELEASE` is set as True.
+"""
+
+import argparse
+import ast
+import sys
+from pathlib import Path
+
+
+def get_release_flag_value(filepath: Path):
+    with open(filepath) as f:
+        fbody = f.read()
+
+    parsed_ast = ast.parse(fbody)
+
+    toplevel_assignments = [
+        node for node in parsed_ast.body if isinstance(node, ast.Assign)
+    ]
+
+    release_val = None
+    for node in toplevel_assignments:
+        if len(node.targets) != 1:
+            continue
+        single_target = node.targets[0]
+        assigned_value = node.value
+        if single_target.id == "_RELEASE":
+            if not isinstance(assigned_value, ast.Constant):
+                raise Exception(
+                    f"Not constant value {assigned_value} is assigned to {single_target}"
+                )
+            release_val = assigned_value.value
+
+    return release_val
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=Path)
+
+    args = parser.parse_args()
+
+    is_release = get_release_flag_value(args.filename)
+
+    if not is_release:
+        print("_RELEASE flag is not set as True")
+        sys.exit(-1)
+
+    sys.exit(0)
