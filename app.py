ed5fdc9717a78e4709aa6cde2baf11d9fa6973bb
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2021-01-16 19:31:24 +0900

    mypy

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pathlib import Path
 try:
     from typing import Literal
 except ImportError:
-    from typing_extensions import Literal
+    from typing_extensions import Literal  # type: ignore
 
 import av
 import cv2
