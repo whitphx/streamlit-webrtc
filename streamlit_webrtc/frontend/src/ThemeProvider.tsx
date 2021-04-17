845e9a75a1a55f271bacc8816738ba4937b0623d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-04-17 15:16:29 +0900

    Theming (#140)
    * Create ThemeProvider to convert Streamlit theme to MaterialUI one
    
    * Install chroma-js to set seconary text color
    
    * Update CHANGELOG

---
+++ streamlit_webrtc/frontend/src/ThemeProvider.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Theme } from "streamlit-component-lib";
+import {
+  createMuiTheme,
+  ThemeProvider as MuiThemeProvider,
+} from "@material-ui/core/styles";
+import chroma from "chroma-js";
+
+interface StreamlitThemeProviderProps {
+  theme: Theme | undefined;
+}
+export const ThemeProvider: React.VFC<
+  React.PropsWithChildren<StreamlitThemeProviderProps>
+> = (props) => {
+  const stTheme = props.theme;
+
+  const muiTheme = React.useMemo(() => {
+    if (stTheme == null) {
+      return undefined;
+    }
+
+    const textColorScale = chroma
+      .scale([stTheme.textColor, stTheme.backgroundColor])
+      .mode("lab");
+
+    return createMuiTheme({
+      palette: {
+        primary: {
+          main: stTheme.primaryColor,
+        },
+        background: {
+          default: stTheme.backgroundColor,
+          paper: stTheme.secondaryBackgroundColor,
+        },
+        text: {
+          primary: stTheme.textColor,
+          secondary: textColorScale(0.1).hex(),
+          disabled: textColorScale(0.5).hex(),
+        },
+      },
+      typography: {
+        fontFamily: stTheme.font,
+      },
+    });
+  }, [stTheme]);
+
+  if (muiTheme == null) {
+    return <>{props.children}</>;
+  }
+
+  return <MuiThemeProvider theme={muiTheme}>{props.children}</MuiThemeProvider>;
+};
+
+export default ThemeProvider;
