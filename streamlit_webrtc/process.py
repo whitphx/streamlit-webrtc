29da6dfcc681b0317992f5dd8837f9edab8d175c
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-05-07 22:22:06 +0900

    Create AsyncAudioProcessTrack

--- streamlit_webrtc/process.py
+++ streamlit_webrtc/process.py
@@ -5,7 +5,8 @@ import queue
 import sys
 import threading
 import traceback
-from typing import Optional, Union
+from collections import deque
+from typing import List, Optional, Union
 
 import av
 import numpy as np
@@ -31,6 +32,10 @@ class AudioProcessorBase(abc.ABC):
     @abc.abstractmethod
     def recv(self, frame: av.AudioFrame) -> av.AudioFrame:
         """ Processes the received frame and returns a new frame """
+
+    @abc.abstractmethod
+    def recv_queued(self, frames: List[av.AudioFrame]) -> av.AudioFrame:
+        """  """
 
 
 VideoProcessor = Union[
@@ -91,7 +96,6 @@ class AsyncVideoProcessTrack(MediaStreamTrack):
         self._in_queue = queue.Queue()
         self._latest_result_img_lock = threading.Lock()
 
-        self._busy = False
         self._latest_result_img: Union[np.ndarray, None] = None
 
         self._thread.start()
@@ -175,3 +179,102 @@ class AudioProcessTrack(MediaStreamTrack):
         new_frame.time_base = frame.time_base
 
         return new_frame
+
+
+# See https://stackoverflow.com/a/42007659
+audio_processing_thread_id_generator = itertools.count()
+
+
+class AsyncAudioProcessTrack(MediaStreamTrack):
+    kind = "audio"
+
+    def __init__(
+        self,
+        track: MediaStreamTrack,
+        audio_processor: AudioProcessorBase,
+        stop_timeout: Optional[float] = None,
+    ):
+        super().__init__()  # don't forget this!
+        self.track = track
+        self.processor = audio_processor
+
+        self._thread = threading.Thread(
+            target=self._run_worker_thread,
+            name=f"async_audio_processor_{next(audio_processing_thread_id_generator)}",
+        )
+        self._in_queue = queue.Queue()
+
+        self._out_lock = threading.Lock()
+        self._out_deque: deque = deque([])
+        self._last_out_frame: Union[av.AudioFrame, None] = None
+
+        self._thread.start()
+
+        self.stop_timeout = stop_timeout
+
+    def _run_worker_thread(self):
+        try:
+            self._worker_thread()
+        except Exception:
+            logger.error("Error occurred in the WebRTC thread:")
+
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            for tb in traceback.format_exception(exc_type, exc_value, exc_traceback):
+                for tbline in tb.rstrip().splitlines():
+                    logger.error(tbline.rstrip())
+
+    def _worker_thread(self):
+        while True:
+            item = self._in_queue.get()
+            if item == __SENTINEL__:
+                break
+
+            queued_frames = [item]
+
+            stop_requested = False
+            while not self._in_queue.empty():
+                item = self._in_queue.get_nowait()
+                if item == __SENTINEL__:
+                    stop_requested = True
+                    break
+                else:
+                    queued_frames.append(item)
+            if stop_requested:
+                break
+
+            if len(queued_frames) == 0:
+                raise Exception("Unexpectedly, queued frames do not exist")
+
+            # TODO: Monitor if execution time grows exponentially
+            new_frames = self.processor.recv_queued(queued_frames)
+
+            with self._out_lock:
+                for new_frame in new_frames:
+                    self._out_deque.append(new_frame)
+
+    def stop(self):
+        self._in_queue.put(__SENTINEL__)
+        self._thread.join(self.stop_timeout)
+
+        return super().stop()
+
+    async def recv(self):
+        frame = await self.track.recv()
+        self._in_queue.put(frame)
+
+        new_frame = None
+        with self._out_lock:
+            if len(self._out_deque) > 0:
+                new_frame = self._out_deque.popleft()
+
+        if new_frame is None:
+            new_frame = self._last_out_frame
+
+        if new_frame:
+            self._last_out_frame = new_frame
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+
+            return new_frame
+
+        return frame
