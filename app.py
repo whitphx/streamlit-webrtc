d287bb8f44c579fb217ad47952838ad2979b1a46
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-05-09 01:03:17 +0900

    Fix audio sample app

--- app.py
+++ app.py
@@ -493,29 +493,42 @@ def app_sendonly_audio():
 
     fig_place = st.empty()
 
-    fig, [ax_time, ax_freq] = plt.subplots(2, 1)
+    fig, [ax_time, ax_freq] = plt.subplots(
+        2, 1, gridspec_kw={"top": 1.5, "bottom": 0.2}
+    )
 
     sound_window_len = 5000  # 5s
     sound_window_buffer = None
     while True:
         if webrtc_ctx.audio_receiver:
-            sound_chunk = pydub.AudioSegment.empty()
             try:
                 audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
             except queue.Empty:
                 logger.warning("Queue is empty. Abort.")
                 break
 
+            sound_chunk = None
             for audio_frame in audio_frames:
+                if sound_chunk is None:
+                    sound_chunk = pydub.AudioSegment.empty()
+                    sound_chunk = (
+                        sound_chunk.set_channels(len(audio_frame.layout.channels))
+                        .set_sample_width(audio_frame.format.bytes)
+                        .set_frame_rate(audio_frame.sample_rate)
+                    )
+
+                data = audio_frame.to_ndarray().reshape(
+                    (len(audio_frame.layout.channels), -1)
+                )
                 sound = pydub.AudioSegment(
-                    data=audio_frame.to_ndarray().tobytes(),
+                    data=data.T.tobytes(),
                     sample_width=audio_frame.format.bytes,
                     frame_rate=audio_frame.sample_rate,
                     channels=len(audio_frame.layout.channels),
                 )
                 sound_chunk += sound
 
-            if len(sound_chunk) > 0:
+            if sound_chunk:
                 if sound_window_buffer is None:
                     sound_window_buffer = pydub.AudioSegment.silent(
                         duration=sound_window_len
@@ -527,11 +540,18 @@ def app_sendonly_audio():
 
             if sound_window_buffer:
                 # Ref: https://own-search-and-study.xyz/2017/10/27/python%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E9%9F%B3%E5%A3%B0%E3%83%87%E3%83%BC%E3%82%BF%E3%81%8B%E3%82%89%E3%82%B9%E3%83%9A%E3%82%AF%E3%83%88%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0%E3%82%92%E4%BD%9C/  # noqa
-                samples = np.array(sound_window_buffer.get_array_of_samples())
-                sample = samples[:: sound_window_buffer.channels]
+                sound_window_buffer = sound_window_buffer.set_channels(
+                    1
+                )  # Stereo to mono
+                sample = np.array(
+                    sound_window_buffer.set_channels(1).get_array_of_samples()
+                )
 
                 ax_time.cla()
-                ax_time.plot(sample[::10])
+                times = (np.arange(-len(sample), 0)) / sound_window_buffer.frame_rate
+                ax_time.plot(times, sample)
+                ax_time.set_xlabel("Time")
+                ax_time.set_ylabel("Magnitude")
 
                 spec = np.fft.fft(sample)
                 freq = np.fft.fftfreq(sample.shape[0], 1.0 / sound_chunk.frame_rate)
@@ -540,8 +560,12 @@ def app_sendonly_audio():
                 spec[0] = spec[0] / 2
 
                 ax_freq.cla()
-                ax_freq.set_yscale("log")
                 ax_freq.plot(freq, np.abs(spec))
+                ax_freq.set_xlabel("Frequency")
+                ax_freq.set_yscale("log")
+                ax_freq.set_ylabel("Magnitude (log)")
+
+                fig.subplots_adjust()
 
                 fig_place.pyplot(fig)
         else:
