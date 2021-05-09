6ddd29352476bde9084390d89c75264e7333ca95
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2021-05-09 17:36:43 +0900

    Fix

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -72,13 +72,15 @@ def download_file(url, download_to: Path, expected_size=None):
 
 
 def main():
-    st.header("Real time Speech-to-Text")
+    st.header("Real Time Speech-to-Text")
     st.markdown(
         """
 This demo app is using [DeepSpeech](https://github.com/mozilla/DeepSpeech),
 an open speech-to-text engine.
 
-A pre-trained model released with v0.9.3, trained on American English is being served.
+A pre-trained model released with
+[v0.9.3](https://github.com/mozilla/DeepSpeech/releases/tag/v0.9.3),
+trained on American English is being served.
 """
     )
 
