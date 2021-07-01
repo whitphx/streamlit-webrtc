e5f99be61727a6cc67229c2309b4de25a90f3505
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-07-01 22:32:09 +0900

    Create a context manager to manage an event loop

--- streamlit_webrtc/eventloop.py
+++ streamlit_webrtc/eventloop.py
@@ -1,4 +1,6 @@
 import asyncio
+import contextlib
+from typing import Union
 
 try:
     from streamlit.server.Server import Server
@@ -10,3 +12,17 @@ except Exception:
 def get_server_event_loop() -> asyncio.AbstractEventLoop:
     current_server = Server.get_current()
     return current_server._ioloop.asyncio_loop
+
+
+@contextlib.contextmanager
+def loop_context(loop: asyncio.AbstractEventLoop):
+    cur_ev_loop: Union[asyncio.AbstractEventLoop, None]
+    try:
+        cur_ev_loop = asyncio.get_event_loop()
+    except RuntimeError:
+        cur_ev_loop = None
+    asyncio.set_event_loop(loop)
+
+    yield
+
+    asyncio.set_event_loop(cur_ev_loop)
--- streamlit_webrtc/relay.py
+++ streamlit_webrtc/relay.py
@@ -1,7 +1,9 @@
 import asyncio
-from typing import Dict, Union
+from typing import Dict
 
 from aiortc.contrib.media import MediaRelay
+
+from .eventloop import loop_context
 
 _relays: Dict[asyncio.AbstractEventLoop, MediaRelay] = dict()
 
@@ -10,16 +12,8 @@ def get_relay(loop: asyncio.AbstractEventLoop) -> MediaRelay:
     global _relays
 
     if loop not in _relays:
-        cur_ev_loop: Union[asyncio.AbstractEventLoop, None]
-        try:
-            cur_ev_loop = asyncio.get_event_loop()
-        except RuntimeError:
-            cur_ev_loop = None
-        asyncio.set_event_loop(loop)
-
-        relay = MediaRelay()
-        _relays[loop] = relay
-
-        asyncio.set_event_loop(cur_ev_loop)
+        with loop_context(loop):
+            relay = MediaRelay()
+            _relays[loop] = relay
 
     return _relays[loop]
