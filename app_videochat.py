4c22b577a63cea034cf1432bcc79eab7bb3b0e4f
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-07-14 22:14:00 +0900

    Update app_videochat.py to apply video filters

--- app_videochat.py
+++ app_videochat.py
@@ -1,17 +1,73 @@
 import logging
 import math
+from collections import OrderedDict
 from typing import List
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # type: ignore
 
 import av
 import cv2
 import numpy as np
+import streamlit as st
 from streamlit_server_state import server_state, server_state_lock
 
-from streamlit_webrtc import ClientSettings, WebRtcMode, webrtc_streamer
-from streamlit_webrtc.factory import create_mux_track
+from streamlit_webrtc import (
+    ClientSettings,
+    VideoProcessorBase,
+    WebRtcMode,
+    webrtc_streamer,
+)
+from streamlit_webrtc.factory import create_mux_track, create_process_track
 from streamlit_webrtc.mux import MuxerBase
 
 logger = logging.getLogger(__name__)
+
+
+class OpenCVVideoProcessor(VideoProcessorBase):
+    type: Literal["noop", "cartoon", "edges", "rotate"]
+
+    def __init__(self) -> None:
+        self.type = "noop"
+
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        img = frame.to_ndarray(format="bgr24")
+
+        if self.type == "noop":
+            pass
+        elif self.type == "cartoon":
+            # prepare color
+            img_color = cv2.pyrDown(cv2.pyrDown(img))
+            for _ in range(6):
+                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+
+            # prepare edges
+            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img_edges = cv2.adaptiveThreshold(
+                cv2.medianBlur(img_edges, 7),
+                255,
+                cv2.ADAPTIVE_THRESH_MEAN_C,
+                cv2.THRESH_BINARY,
+                9,
+                2,
+            )
+            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+
+            # combine color and edges
+            img = cv2.bitwise_and(img_color, img_edges)
+        elif self.type == "edges":
+            # perform edge detection
+            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+        elif self.type == "rotate":
+            # rotate image
+            rows, cols, _ = img.shape
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+            img = cv2.warpAffine(img, M, (cols, rows))
+
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
 class MultiWindowMuxer(MuxerBase):
@@ -62,7 +118,7 @@ class MultiWindowMuxer(MuxerBase):
 
 def main():
     if "webrtc_contexts" not in server_state:
-        server_state["webrtc_contexts"] = []
+        server_state["webrtc_contexts"] = OrderedDict()
 
     mux_track = create_mux_track(
         kind="video", muxer_factory=MultiWindowMuxer, key="mux"
@@ -81,25 +137,35 @@ def main():
         sendback_audio=False,
     )
 
+    self_process_track = None
     if self_ctx.input_video_track:
-        mux_track.add_input_track(self_ctx.input_video_track)
+        self_process_track = create_process_track(
+            input_track=self_ctx.input_video_track,
+            processor_factory=OpenCVVideoProcessor,
+        )
+        mux_track.add_input_track(self_process_track)
+
+        self_process_track.processor.type = st.radio(
+            "Select transform type",
+            ("noop", "cartoon", "edges", "rotate"),
+            key="filter1-type",
+        )
 
     with server_state_lock["webrtc_contexts"]:
-        webrtc_contexts = server_state["webrtc_contexts"]
-        if self_ctx.state.playing and self_ctx not in webrtc_contexts:
-            webrtc_contexts.append(self_ctx)
+        webrtc_contexts: OrderedDict = server_state["webrtc_contexts"]
+        self_is_playing = self_ctx.state.playing and self_process_track
+        if self_is_playing and self_ctx not in webrtc_contexts:
+            webrtc_contexts[self_ctx] = self_process_track
             server_state["webrtc_contexts"] = webrtc_contexts
-        elif not self_ctx.state.playing and self_ctx in webrtc_contexts:
-            webrtc_contexts.remove(self_ctx)
+        elif not self_is_playing and self_ctx in webrtc_contexts:
+            del webrtc_contexts[self_ctx]
             server_state["webrtc_contexts"] = webrtc_contexts
 
-    active_other_ctxs = [
-        ctx for ctx in webrtc_contexts if ctx != self_ctx and ctx.state.playing
-    ]
-
-    for ctx in active_other_ctxs:
+    for ctx, track in webrtc_contexts.items():
+        if ctx == self_ctx or not ctx.state.playing:
+            continue
         # Video streams are handled in MCU manner
-        mux_track.add_input_track(ctx.input_video_track)
+        mux_track.add_input_track(track)
         # Audio streams are transferred in SFU manner
         # TODO: Create MCU to mix audio streams
         webrtc_streamer(
