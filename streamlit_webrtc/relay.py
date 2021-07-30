4a8b1dc0852fd3c01e1e195503a91c1ca4f2fdd5
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-07-30 21:03:56 +0900

    Fix comment

--- streamlit_webrtc/relay.py
+++ streamlit_webrtc/relay.py
@@ -5,10 +5,10 @@ from .eventloop import get_server_event_loop, loop_context
 
 _SERVER_GLOBAL_RELAY_ATTR_NAME_ = "streamlit-webrtc-global-relay"
 
-# NOTE: Accessing the server only when it is necessary (in `get_global_relay()`)
-#       is important to be compatible with multiprocessing because
-#       `Server.get_current()` always raises an error inside a forked process
-#       saying that the server has not been initialized.
+# NOTE: Accessing the server object only when it is necessary (in `get_global_relay()`)
+#       is important for this module to be compatible with multiprocessing because
+#       in a forked process `Server.get_current()` always raises an error
+#       as the Streamlit (actually Tornado) server does not exist in that process.
 #       See https://github.com/whitphx/streamlit-webrtc/issues/354
 _server = None
 
