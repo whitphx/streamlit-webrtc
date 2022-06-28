f3262f716a4da687efd8e3679d72f995fcfd589d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-06-28 11:11:51 +0900

    Fix app_deepspeech.py to use class-less callback (#939)

--- app_deepspeech.py
+++ app_deepspeech.py
@@ -13,7 +13,7 @@ import numpy as np
 import pydub
 import streamlit as st
 
-from streamlit_webrtc import AudioProcessorBase, WebRtcMode, webrtc_streamer
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
 HERE = Path(__file__).parent
 
@@ -173,35 +173,32 @@ def app_sst(model_path: str, lm_path: str, lm_alpha: float, lm_beta: float, beam
 def app_sst_with_video(
     model_path: str, lm_path: str, lm_alpha: float, lm_beta: float, beam: int
 ):
-    class AudioProcessor(AudioProcessorBase):
-        frames_lock: threading.Lock
-        frames: deque
+    frames_deque_lock = threading.Lock()
+    frames_deque: deque = deque([])
 
-        def __init__(self) -> None:
-            self.frames_lock = threading.Lock()
-            self.frames = deque([])
+    async def queued_audio_frames_callback(
+        frames: List[av.AudioFrame],
+    ) -> av.AudioFrame:
+        with frames_deque_lock:
+            frames_deque.extend(frames)
 
-        async def recv_queued(self, frames: List[av.AudioFrame]) -> av.AudioFrame:
-            with self.frames_lock:
-                self.frames.extend(frames)
+        # Return empty frames to be silent.
+        new_frames = []
+        for frame in frames:
+            input_array = frame.to_ndarray()
+            new_frame = av.AudioFrame.from_ndarray(
+                np.zeros(input_array.shape, dtype=input_array.dtype),
+                layout=frame.layout.name,
+            )
+            new_frame.sample_rate = frame.sample_rate
+            new_frames.append(new_frame)
 
-            # Return empty frames to be silent.
-            new_frames = []
-            for frame in frames:
-                input_array = frame.to_ndarray()
-                new_frame = av.AudioFrame.from_ndarray(
-                    np.zeros(input_array.shape, dtype=input_array.dtype),
-                    layout=frame.layout.name,
-                )
-                new_frame.sample_rate = frame.sample_rate
-                new_frames.append(new_frame)
-
-            return new_frames
+        return new_frames
 
     webrtc_ctx = webrtc_streamer(
         key="speech-to-text-w-video",
         mode=WebRtcMode.SENDRECV,
-        audio_processor_factory=AudioProcessor,
+        queued_audio_frames_callback=queued_audio_frames_callback,
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
         media_stream_constraints={"video": True, "audio": True},
     )
@@ -216,7 +213,7 @@ def app_sst_with_video(
     stream = None
 
     while True:
-        if webrtc_ctx.audio_processor:
+        if webrtc_ctx.state.playing:
             if stream is None:
                 from deepspeech import Model
 
@@ -232,9 +229,9 @@ def app_sst_with_video(
             sound_chunk = pydub.AudioSegment.empty()
 
             audio_frames = []
-            with webrtc_ctx.audio_processor.frames_lock:
-                while len(webrtc_ctx.audio_processor.frames) > 0:
-                    frame = webrtc_ctx.audio_processor.frames.popleft()
+            with frames_deque_lock:
+                while len(frames_deque) > 0:
+                    frame = frames_deque.popleft()
                     audio_frames.append(frame)
 
             if len(audio_frames) == 0:
@@ -262,7 +259,7 @@ def app_sst_with_video(
                 text = stream.intermediateDecode()
                 text_output.markdown(f"**Text:** {text}")
         else:
-            status_indicator.write("AudioReciver is not set. Abort.")
+            status_indicator.write("Stopped.")
             break
 
 
