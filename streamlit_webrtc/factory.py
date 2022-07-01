aef720c00aaa8a0cd7d4bb8e2c1dd43321407fdb
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-07-01 15:08:50 +0900

    Fix create_process_track() API to be functional-callback-based

--- streamlit_webrtc/factory.py
+++ streamlit_webrtc/factory.py
@@ -1,4 +1,4 @@
-from typing import Generic, Hashable, TypeVar, Union, overload
+from typing import Optional, Union, overload
 
 try:
     from typing import Literal
@@ -10,7 +10,13 @@ from aiortc import MediaStreamTrack
 
 from .eventloop import get_server_event_loop, loop_context
 from .mix import MediaStreamMixTrack, MixerCallback
-from .models import FrameT
+from .models import (
+    CallbackAttachableProcessor,
+    FrameCallback,
+    FrameT,
+    MediaEndedCallback,
+    QueuedVideoFramesCallback,
+)
 from .process import (
     AsyncAudioProcessTrack,
     AsyncMediaProcessTrack,
@@ -26,51 +32,6 @@ from .webrtc import (
     VideoProcessorFactory,
     VideoProcessorT,
 )
-
-HashedObjT = TypeVar("HashedObjT")
-
-
-# NOTE: Workaround of dealing with hash_funcs of st.cache.
-# See https://discuss.streamlit.io/t/how-to-specify-hash-funcs-of-st-cache-for-all-subtypes-of-a-specific-class/14535  # noqa: E501
-class ObjectHashWrapper(Generic[HashedObjT]):
-    def __init__(self, obj: HashedObjT, hash: Hashable) -> None:
-        self.obj = obj
-        self.hash = hash
-
-
-@st.cache(hash_funcs={ObjectHashWrapper: lambda o: o.hash})
-def _inner_create_process_track(
-    wrapped_input_track: ObjectHashWrapper[MediaStreamTrack],
-    wrapped_processor_factory: ObjectHashWrapper[
-        Union[VideoProcessorFactory, AudioProcessorFactory]
-    ],
-    async_processing: bool,
-) -> ObjectHashWrapper[Union[AsyncMediaProcessTrack, MediaProcessTrack]]:
-    input_track = wrapped_input_track.obj
-    processor_factory = wrapped_processor_factory.obj
-
-    processor = processor_factory()
-
-    Track: MediaStreamTrack
-    if input_track.kind == "video":
-        if async_processing:
-            Track = AsyncVideoProcessTrack
-        else:
-            Track = VideoProcessTrack
-    elif input_track.kind == "audio":
-        if async_processing:
-            Track = AsyncAudioProcessTrack
-        else:
-            Track = AudioProcessTrack
-    else:
-        raise ValueError(f"Unsupported track type: {input_track.kind}")
-
-    loop = get_server_event_loop()
-    relay = get_global_relay()
-    with loop_context(loop):
-        output_track = Track(relay.subscribe(input_track), processor)
-
-    return ObjectHashWrapper(output_track, output_track.id)
 
 
 @overload
@@ -109,17 +70,56 @@ def create_process_track(
     pass
 
 
+_PROCESSOR_TRACK_CACHE_KEY_PREFIX = "__PROCESSOR_TRACK_CACHE__"
+
+
+def _get_track_class(
+    kind: Literal["video", "audio"], async_processing: bool
+) -> MediaStreamTrack:
+    if kind == "video":
+        if async_processing:
+            return AsyncVideoProcessTrack
+        else:
+            return VideoProcessTrack
+    elif kind == "audio":
+        if async_processing:
+            return AsyncAudioProcessTrack
+        else:
+            return AudioProcessTrack
+    else:
+        raise ValueError(f"Unsupported track type: {kind}")
+
+
 def create_process_track(
     input_track,
-    processor_factory,
+    frame_callback: Optional[FrameCallback] = None,
+    queued_frames_callback: Optional[QueuedVideoFramesCallback] = None,
+    on_ended: Optional[MediaEndedCallback] = None,
     async_processing=True,
-):
-    wrapped_input_track = ObjectHashWrapper(input_track, input_track.id)
-    wrapped_processor_factory = ObjectHashWrapper(processor_factory, None)
-    wrapped_output_track = _inner_create_process_track(
-        wrapped_input_track, wrapped_processor_factory, async_processing
-    )
-    return wrapped_output_track.obj
+) -> Union[MediaProcessTrack, AsyncMediaProcessTrack]:
+    cache_key = _PROCESSOR_TRACK_CACHE_KEY_PREFIX + str(input_track.id)
+    if cache_key in st.session_state:
+        processor_track = st.session_state[cache_key]
+        processor: CallbackAttachableProcessor = processor_track.processor
+        processor.update_callbacks(
+            frame_callback=frame_callback,
+            queued_frames_callback=queued_frames_callback,
+            ended_callback=on_ended,
+        )
+    else:
+        processor = CallbackAttachableProcessor(
+            frame_callback=frame_callback,
+            queued_frames_callback=queued_frames_callback,
+            ended_callback=on_ended,
+        )
+        Track = _get_track_class(input_track.kind, async_processing)
+        loop = get_server_event_loop()
+        relay = get_global_relay()
+        with loop_context(loop):
+            processor_track = Track(relay.subscribe(input_track), processor)
+            st.session_state[cache_key] = processor_track
+
+    return processor_track
 
 
 _MIXER_TRACK_CACHE_KEY_PREFIX = "__MIXER_TRACK_CACHE__"
