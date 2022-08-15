f11c19fb9bd666434cebbdd71e5d51857100878e
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-15 12:17:12 +0900

    Fix import

--- tests/session_info_test.py
+++ tests/session_info_test.py
@@ -1,10 +1,10 @@
 from unittest.mock import Mock
 
-from streamlit.server.server import SessionInfo
+from streamlit.web.server.server import SessionInfo
 
 from streamlit_webrtc.session_info import get_script_run_count
 
 
 def test_get_script_run_count():
-    session_info = SessionInfo(ws=Mock(), session=Mock())
+    session_info = SessionInfo(client=Mock(), session=Mock())
     assert get_script_run_count(session_info) == 0
