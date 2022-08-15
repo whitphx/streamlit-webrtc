f1dec2ea6d3e504dd6a5a11009291c6033670aa3
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-15 12:22:31 +0900

    Fix import

--- tests/session_info_test.py
+++ tests/session_info_test.py
@@ -1,8 +1,6 @@
 from unittest.mock import Mock
 
-from streamlit.web.server.server import SessionInfo
-
-from streamlit_webrtc.session_info import get_script_run_count
+from streamlit_webrtc.session_info import SessionInfo, get_script_run_count
 
 
 def test_get_script_run_count():
