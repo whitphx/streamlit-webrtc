1a3f39d1cb45dee5ea7024bbe20a903b987583ea
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-15 12:41:22 +0900

    Fix initialization of SessionInfo in a test

--- tests/session_info_test.py
+++ tests/session_info_test.py
@@ -1,8 +1,12 @@
 from unittest.mock import Mock
 
+from streamlit_webrtc.server import _is_modern_architecture
 from streamlit_webrtc.session_info import SessionInfo, get_script_run_count
 
 
 def test_get_script_run_count():
-    session_info = SessionInfo(client=Mock(), session=Mock())
+    if _is_modern_architecture():
+        session_info = SessionInfo(client=Mock(), session=Mock())
+    else:
+        session_info = SessionInfo(ws=Mock(), session=Mock())
     assert get_script_run_count(session_info) == 0
