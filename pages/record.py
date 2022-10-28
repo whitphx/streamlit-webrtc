8f14cc16caf5b75f7a1e8e9bb721ecca8cab24ad
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-10-28 14:06:04 +0900

    Fix the webrtc_streamer() key in the recording example

--- pages/record.py
+++ pages/record.py
@@ -37,7 +37,7 @@ def app():
         return MediaRecorder(str(out_file), format="flv")
 
     webrtc_streamer(
-        key="loopback",
+        key="record",
         mode=WebRtcMode.SENDRECV,
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
         media_stream_constraints={
