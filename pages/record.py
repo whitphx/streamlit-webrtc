d1f8c01cd1afef5b0bea3e3807be659e1674d753
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-10-28 14:04:44 +0900

    Set the session specific file names of the recorded videos (#1115)

--- pages/record.py
+++ pages/record.py
@@ -1,5 +1,9 @@
+import uuid
+from pathlib import Path
+
 import av
 import cv2
+import streamlit as st
 from aiortc.contrib.media import MediaRecorder
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
@@ -13,14 +17,24 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
+RECORD_DIR = Path("./records")
+RECORD_DIR.mkdir(exist_ok=True)
+
+
 def app():
+    if "prefix" not in st.session_state:
+        st.session_state["prefix"] = str(uuid.uuid4())
+    prefix = st.session_state["prefix"]
+    in_file = RECORD_DIR / f"{prefix}_input.flv"
+    out_file = RECORD_DIR / f"{prefix}_output.flv"
+
     def in_recorder_factory() -> MediaRecorder:
         return MediaRecorder(
-            "input.flv", format="flv"
+            str(in_file), format="flv"
         )  # HLS does not work. See https://github.com/aiortc/aiortc/issues/331
 
     def out_recorder_factory() -> MediaRecorder:
-        return MediaRecorder("output.flv", format="flv")
+        return MediaRecorder(str(out_file), format="flv")
 
     webrtc_streamer(
         key="loopback",
@@ -34,6 +48,17 @@ def app():
         in_recorder_factory=in_recorder_factory,
         out_recorder_factory=out_recorder_factory,
     )
+
+    if in_file.exists():
+        with in_file.open("rb") as f:
+            st.download_button(
+                "Download the recorded video without video filter", f, "input.flv"
+            )
+    if out_file.exists():
+        with out_file.open("rb") as f:
+            st.download_button(
+                "Download the recorded video with video filter", f, "output.flv"
+            )
 
 
 if __name__ == "__main__":
