a61ff0d39b35334a287aa51d8696d0783dec1954
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-10-28 10:34:26 +0900

    Fix app_record.py to use the class-less callback

--- app_record.py
+++ app_record.py
@@ -2,19 +2,19 @@ import av
 import cv2
 from aiortc.contrib.media import MediaRecorder
 
-from streamlit_webrtc import VideoProcessorBase, WebRtcMode, webrtc_streamer
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+
+
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    img = frame.to_ndarray(format="bgr24")
+
+    # perform edge detection
+    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
 def app():
-    class OpenCVEdgeProcessor(VideoProcessorBase):
-        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-            img = frame.to_ndarray(format="bgr24")
-
-            # perform edge detection
-            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
-
     def in_recorder_factory() -> MediaRecorder:
         return MediaRecorder(
             "input.flv", format="flv"
@@ -31,7 +31,7 @@ def app():
             "video": True,
             "audio": True,
         },
-        video_processor_factory=OpenCVEdgeProcessor,
+        video_frame_callback=video_frame_callback,
         in_recorder_factory=in_recorder_factory,
         out_recorder_factory=out_recorder_factory,
     )
