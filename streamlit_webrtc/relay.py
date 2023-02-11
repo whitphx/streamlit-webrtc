9b9cd10726542625260b5787a28b36d972424acc
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-02-11 14:46:05 +0900

    Fix relay.py

--- streamlit_webrtc/relay.py
+++ streamlit_webrtc/relay.py
@@ -1,7 +1,7 @@
 from aiortc.contrib.media import MediaRelay
 
 from .eventloop import get_global_event_loop, loop_context
-from .server import VER_GTE_1_14_0, get_current_server
+from .server import VER_GTE_1_12_1, VER_GTE_1_14_0, get_current_server
 
 _SERVER_GLOBAL_RELAY_ATTR_NAME_ = "streamlit-webrtc-global-relay"
 
@@ -11,6 +11,8 @@ def get_global_relay() -> MediaRelay:
         from streamlit.runtime.runtime import Runtime
 
         singleton = Runtime.instance()  # type: ignore
+    elif VER_GTE_1_12_1:
+        singleton = get_current_server()._runtime
     else:
         singleton = get_current_server()
 
