5014e3ccd5bbf1359afebd455d203177e852fe75
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-09-10 21:23:02 +0800

    Remove @st.cache_data on get_ice_servers() because the ICE servers data may change (#1375)

--- sample_utils/turn.py
+++ sample_utils/turn.py
@@ -8,7 +8,6 @@ from twilio.rest import Client
 logger = logging.getLogger(__name__)
 
 
-@st.cache_data  # type: ignore
 def get_ice_servers():
     """Use Twilio's TURN server because Streamlit Community Cloud has changed
     its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
