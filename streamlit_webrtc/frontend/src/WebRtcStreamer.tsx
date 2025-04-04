08d21bc61011c596ac70327914ffe6d79ee268c3
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-04-04 13:51:00 +0900

    lint

--- streamlit_webrtc/frontend/src/WebRtcStreamer.tsx
+++ streamlit_webrtc/frontend/src/WebRtcStreamer.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useCallback } from "react";
 import Box from "@mui/material/Box";
 import Alert from "@mui/material/Alert";
 import DeviceSelectForm from "./DeviceSelect/DeviceSelectForm";
