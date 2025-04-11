e397ef1bfe3f1120080cd3d05baa11dccc8f1242
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-04-11 14:47:23 +0900

    fix to use ref

--- streamlit_webrtc/frontend/src/webrtc/use-unique-id.ts
+++ streamlit_webrtc/frontend/src/webrtc/use-unique-id.ts
@@ -1,17 +1,17 @@
-import { useState, useCallback } from "react";
+import { useRef, useCallback } from "react";
 
 export function useUniqueId() {
-  const [uniqueIds, setUniqueIds] = useState(new Set<string>());
+  const uniqueIds = useRef(new Set<string>());
   const getUniqueId = useCallback(() => {
     let id;
     do {
       id = Math.random().toString(36).substring(2, 15);
-    } while (uniqueIds.has(id));
-    setUniqueIds(new Set(uniqueIds).add(id));
+    } while (uniqueIds.current.has(id));
+    uniqueIds.current.add(id);
     return id;
   }, [uniqueIds]);
   const resetUniqueIds = useCallback(() => {
-    setUniqueIds(new Set<string>());
+    uniqueIds.current = new Set<string>();
   }, []);
   return { get: getUniqueId, reset: resetUniqueIds };
 }
